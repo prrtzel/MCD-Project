3e7899c44226665c23724099e803012856ec60f7
prrtzel <gsyacoubian@gmail.com>
2023-12-12 08:49:06 -0500

    cleanup

--- ASM/test/src/assembly/main.c
+++ ASM/test/src/assembly/main.c
@@ -2,11 +2,8 @@ extern void sprintTest(void);
 extern void sprint(char*string);
 
 int main(){
-    char string[50] = "hi"; 
-
-    for(int i=0;i<100;i++){
-        sprint(string);
-    }
-
+    char str[20] = "hello world";
+    sprint(str); 
     return 9;
 }
+
