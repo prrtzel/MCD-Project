a16449d4ba76b4aebcf64f61be79e4a0a9501df2
prrtzel <gsyacoubian@gmail.com>
2024-06-17 08:36:41 -0400

    correction, data section works, bss not set to 0

--- Rev2/OS/morg/src/main.c
+++ Rev2/OS/morg/src/main.c
@@ -7,17 +7,11 @@ const char fl[] = "big endian";
 const char str[] = "fale";
 
 long two = 0x5678;
-
-__attribute__((section(".bss")))
-long three;
-long one = 0x1234;
-
 char y = 'Y';
 
+
+
 int __main() {
-    one++;
-    serial_print("one: ");
-    binary_to_ascii_hex(one, input_buffer, HEX_LONG_LENGTH);
-    serial_print(&input_buffer[0]);
+
     return 0;
 }
